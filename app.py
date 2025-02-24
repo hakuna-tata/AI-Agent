3eff3dac8377a5ea2b347a555b042e0cfee605a7
hakuna-tata <610578197@qq.com>
2025-02-24 15:29:25 +0800

    feat: 完整链路

--- app.py
+++ app.py
@@ -1,29 +1,51 @@
 import os
-from flask import Flask
+from dotenv import load_dotenv
+from flask import Flask, request, send_file, send_from_directory
 from voice_processing import VoiceProcessor
 
 app = Flask(__name__)
-processor = VoiceProcessor('117689738', 'i570W6mSVYwiyzVTCqDiHro8', 'n2F9vbzUfIXSpiNGdXsY6sSNcsf4pHPW')
+
+load_dotenv()
+processor = VoiceProcessor(
+    os.getenv('AipSpeech_APP_ID'),
+    os.getenv('AipSpeech_API_KEY'),
+    os.getenv('AipSpeech_SECRET_KEY'),
+    os.getenv('OPENAI_API_KEY')
+)
 
 @app.after_request
 def add_cors_headers(response):
     response.headers['Access-Control-Allow-Origin'] = '*'
-    response.headers['Access-Control-Allow-Methods'] = 'GET, POST'
+    response.headers['Access-Control-Allow-Methods'] = 'POST'
 
     return response
 
+@app.route('/')
+def serve_index():
+    return send_from_directory('.', 'index.html')
 
-@app.route('/voice_process', methods=['GET'])
+@app.route('/voice_process', methods=['POST'])
 def voice_process():
     try:
-        # file_path = os.path.join(os.getcwd(), 'test.wav')
-        answer_wav_path = processor.process_voice(file_path)
-        if answer_wav_path:
-            return answer_wav_path
+        voice_file = request.files['voice']
+        voice_path = 'received_voice.wav'
+        voice_file.save(voice_path)
+        answer_mp3_path = processor.voice_process_chain(voice_path)
+        if answer_mp3_path:
+            return send_file(answer_mp3_path, mimetype='audio/mp3')
         else:
             return "语音处理失败", 500
     except Exception as e:
         return str(e), 500
+    finally:
+        if os.path.exists('received_voice.wav'):
+            os.remove('received_voice.wav')
+        if os.path.exists('answer_voice.mp3'):
+            os.remove('answer_voice.mp3')
+        if os.path.exists('answer_voice.wav'):
+            os.remove('answer_voice.wav')
+        if os.path.exists('answer_voice.mp3'):
+            os.remove('answer_voice.mp3')
 
 if __name__ == '__main__':
     app.run(debug=True)
--- voice_processing.py
+++ voice_processing.py
@@ -1,36 +1,39 @@
+import os
+import time
 from aip import AipSpeech
+from openai import OpenAI
 
 class VoiceProcessor:
-    def __init__(self, app_id, api_key, secret_key):
-        self.speech_client = AipSpeech(app_id, api_key, secret_key)
-
-    def process_voice(self, voice_file_path):
-        try:
-            with open(voice_file_path, 'rb') as f:
-                audio_data = f.read()
-            result = self.speech_client.asr(audio_data, 'wav', 16000, {
-                'dev_pid': 1537,
-            })
-            if result['err_no'] == 0:
-                text = result['result'][0]
-                return text
-            else:
-                raise Exception("语音识别失败")
-        except Exception as e:
-            print(f"处理语音时出错: {e}")
-        
-            return None
+    def __init__(self, app_id, api_key, secret_key, openai_key):
+        self.speech_client = AipSpeech(
+            app_id,
+            api_key,
+            secret_key
+        )
+        self.openai_client = OpenAI(
+            api_key=openai_key,
+            base_url='https://dashscope.aliyuncs.com/compatible-mode/v1'
+        )
+        self.messages = [
+            {
+                "role": "system",
+                "content": "你是一个智能助手，能够回答用户问题并且解决问题，给用户提供情绪价值。"
+            }
+        ]
         
     def parse_voice(self, voice_file_path):
         try:
+            start_time = time.time()
             with open(voice_file_path, 'rb') as f:
                 audio_data = f.read()
             result = self.speech_client.asr(audio_data, 'wav', 16000, {
                 'dev_pid': 1537,
             })
             if result['err_no'] == 0:
-                text = result['result'][0]
-                return text
+                end_time = time.time()
+                print("语音解析结果:", result['result'][0])
+                print(f"语音解析耗时: {end_time - start_time:.4f} 秒")
+                return result['result'][0]
             else:
                 raise Exception("语音识别失败")
         except Exception as e:
@@ -38,8 +41,52 @@ class VoiceProcessor:
         
             return None
         
-    def aiChat(self):
-        return None
+    def aiChat(self, content):
+        start_time = time.time()
+        self.messages.append({"role": "user", "content": content})
+        try:
+            completion = self.openai_client.chat.completions.create(
+                model="qwen-turbo",
+                messages=self.messages
+            )
+            assistant_output = completion.choices[0].message.content
+            self.messages.append({"role": "assistant", "content": assistant_output})
+            end_time = time.time()
+            print("大模型回答结果:", assistant_output)
+            print(f"大模型对话耗时: {end_time - start_time:.4f} 秒")
+            return assistant_output
+        except Exception as e:
+            print(f"大模型处理时出错: {e}")
+        
+            return None 
 
-    def generate_voice(self):
-        return None
+    def generate_voice(self, answer):
+        try:
+            start_time = time.time()
+            result = self.speech_client.synthesis(answer, 'zh', 1, {
+                'vol': 5,
+                
+            })
+            if not isinstance(result, dict):
+                answer_mp3_path = 'answer_voice.mp3'
+                with open(answer_mp3_path, 'wb') as f:
+                    f.write(result)
+                end_time = time.time()
+                print(f"语音合成耗时: {end_time - start_time:.4f} 秒")
+                return answer_mp3_path
+            else:
+                raise Exception("语音合成失败")
+        except Exception as e:
+            print(f"处理语音时出错: {e}")
+            return None
+
+    def voice_process_chain(self, voice_file_path):
+        try:
+            client_voice_conetent=self.parse_voice(voice_file_path)
+            ai_chat_content=self.aiChat(client_voice_conetent)
+            generate_content_audio=self.generate_voice(ai_chat_content)
+            return generate_content_audio
+        
+        except Exception as e:
+            print(f"流程链式调用失败: {e}")
+            return None
